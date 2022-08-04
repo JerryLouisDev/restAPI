35fb3f2eb38f8aadb8ae993f4e8a223ad6abee57
Jēy Löuie <jeylouie@JeyBook-Pro.local>
2022-08-04 16:36:34 -0700

    adding Models

---
+++ models/Course.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const { Model, DataTypes } = require('sequelize');
+const bcrpyt = require('bcryptjs');
+// const sequelize = require('sequelize');
+
+module.exports = (sequelize) => {
+
+    class User extends Model{}
+
+}
